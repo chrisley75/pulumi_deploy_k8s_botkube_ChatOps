96f66be854325a6018d058869cbea9a33934d3a5
chrisley75 <christopher.ley@gmail.com>
2020-09-10 10:45:02 +0200

    edit __main__.py

--- __main__.py
+++ __main__.py
@@ -1,19 +1,32 @@
-"""A Kubernetes Python Pulumi program"""
+#https://www.pulumi.com/docs/get-started/kubernetes/modify-program/
 
 import pulumi
 from pulumi_kubernetes.apps.v1 import Deployment
+from pulumi_kubernetes.core.v1 import Service
+from pulumi_kubernetes.helm.v3 import Chart, ChartOpts, FetchOpts
 
-app_labels = { "app": "nginx" }
+# Deploy BOTKube chatops
 
-deployment = Deployment(
-    "nginx",
-    spec={
-        "selector": { "match_labels": app_labels },
-        "replicas": 1,
-        "template": {
-            "metadata": { "labels": app_labels },
-            "spec": { "containers": [{ "name": "nginx", "image": "nginx" }] }
-        }
-    })
+botkube = Chart(
+   'botkube', 
+   ChartOpts(
+     repo='infracloudio',
+     chart='botkube',
+     version='v0.10.0',
+     namespace='botkube',
+#     fetch_opts=FetchOpts(
+#         repo="https://infracloudio.github.io/charts",
+#     ),
+     values={
+       'communications.slack.enabled': 'true',
+       'communications.slack.channel': 'chatops',
+       'communications.slack.token': 'xoxb-1349221428866-1361852017825-hmvgOOdtb71FmCPWngAjjN9H',
+       'config.settings.clustername': 'akspulu',
+       'config.settings.allowkubectl': 'true' 
+     },
+    ),
+   )
+
+
 
 pulumi.export("name", deployment.metadata["name"])
